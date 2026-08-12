ea9cbbbaa80d30c2b5cc15fdacca7a4f7b008471
Jonas <89553471+JonasFont@users.noreply.github.com>
2026-08-12 11:59:37 -0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,6 +1,5 @@
 // js/firebase-config.js
 
-// Suas credenciais do Console do Firebase
 const firebaseConfig = {
     apiKey: "AIzaSyDNfajgndGJ5KJgM2-uN26dNQ6U0EAP1Pk",
     authDomain: "jonasxp-eb80e.firebaseapp.com",
@@ -9,10 +8,12 @@ const firebaseConfig = {
     messagingSenderId: "648665033902",
     appId: "1:648665033902:web:abc1d5806fa7c6ffafd13f"
 };
-// Inicializa o Firebase
+
+// Inicializa o Firebase apenas se ainda não tiver sido inicializado
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
 }
 
-// Exporta o banco Firestore
-const db = firebase.firestore();
+// Inicializa o Firestore e torna disponível tanto globalmente quanto por exportação
+export const db = firebase.firestore();
+window.db = db;
